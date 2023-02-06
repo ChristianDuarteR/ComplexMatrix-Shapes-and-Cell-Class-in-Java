0542d6c0cba7d284b2f01dea4f2811618ace3685
Christian Javier Duarte <112821120+Chris-DuJARo@users.noreply.github.com>
2023-02-06 16:10:47 -0500

    Add files via upload

--- POOB/shapes/Circle.java
+++ POOB/shapes/Circle.java
@@ -8,15 +8,11 @@ import java.awt.geom.*;
  * @version 1.0.  (15 July 2000) 
  */
 
-public class Circle{
+public class Circle extends Figure{
 
     public static final double PI=3.1416;
     
     protected double diameter;
-    protected int xPosition;
-    protected int yPosition;
-    protected String color;
-    protected boolean isVisible;
     
 
     public Circle(){
@@ -26,21 +22,9 @@ public class Circle{
         color = "blue";
         isVisible = false;
     }
-
-
-       
-    public void makeVisible(){
-        isVisible = true;
-        draw();
-    }
     
 
-    public void makeInvisible(){
-        erase();
-        isVisible = false;
-    }
-
-    private void draw(){
+    public void draw(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
             canvas.draw(this, color, 
@@ -50,98 +34,10 @@ public class Circle{
         }
     }
 
-    private void erase(){
+    public void erase(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
             canvas.erase(this);
-        }
-    }
-    
-    /**
-     * Move the circle a few pixels to the right.
-     */
-    public void moveRight(){
-        moveHorizontal(20);
-    }
-
-    /**
-     * Move the circle a few pixels to the left.
-     */
-    public void moveLeft(){
-        moveHorizontal(-20);
-    }
-
-    /**
-     * Move the circle a few pixels up.
-     */
-    public void moveUp(){
-        moveVertical(-20);
-    }
-
-    /**
-     * Move the circle a few pixels down.
-     */
-    public void moveDown(){
-        moveVertical(20);
-    }
-
-    /**
-     * Move the circle horizontally.
-     * @param distance the desired distance in pixels
-     */
-    public void moveHorizontal(int distance){
-        erase();
-        xPosition += distance;
-        draw();
-    }
-
-    /**
-     * Move the circle vertically.
-     * @param distance the desired distance in pixels
-     */
-    public void moveVertical(int distance){
-        erase();
-        yPosition += distance;
-        draw();
-    }
-
-    /**
-     * Slowly move the circle horizontally.
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveHorizontal(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        } else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            xPosition += delta;
-            draw();
-        }
-    }
-
-    /**
-     * Slowly move the circle vertically
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveVertical(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        }else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            yPosition += delta;
-            draw();
         }
     }
 
@@ -152,16 +48,6 @@ public class Circle{
     public void changeSize(int newDiameter){
         erase();
         diameter = newDiameter;
-        draw();
-    }
-
-    /**
-     * Change the color. 
-     * @param color the new color. Valid colors are "red", "yellow", "blue", "green",
-     * "magenta" and "black".
-     */
-    public void changeColor(String newColor){
-        color = newColor;
         draw();
     }
     
--- POOB/shapes/Triangle.java
+++ POOB/shapes/Triangle.java
@@ -7,17 +7,12 @@ import java.awt.*;
  * @version 1.0  (15 July 2000)
  */
 
-public class Triangle{
+public class Triangle extends Figure{
     
     public static int VERTICES=3;
     
     protected int height;
     protected int width;
-    protected int xPosition;
-    protected int yPosition;
-    protected String color;
-    protected boolean isVisible;
-
     /**
      * Create a new triangle at default position with default color.
      */
@@ -30,109 +25,6 @@ public class Triangle{
         isVisible = false;
     }
 
-    /**
-     * Make this triangle visible. If it was already visible, do nothing.
-     */
-    public void makeVisible(){
-        isVisible = true;
-        draw();
-    }
-    
-    /**
-     * Make this triangle invisible. If it was already invisible, do nothing.
-     */
-    public void makeInvisible(){
-        erase();
-        isVisible = false;
-    }
-    
-    /**
-     * Move the triangle a few pixels to the right.
-     */
-    public void moveRight(){
-        moveHorizontal(20);
-    }
-
-    /**
-     * Move the triangle a few pixels to the left.
-     */
-    public void moveLeft(){
-        moveHorizontal(-20);
-    }
-
-    /**
-     * Move the triangle a few pixels up.
-     */
-    public void moveUp(){
-        moveVertical(-20);
-    }
-
-    /**
-     * Move the triangle a few pixels down.
-     */
-    public void moveDown(){
-        moveVertical(20);
-    }
-
-    /**
-     * Move the triangle horizontally.
-     * @param distance the desired distance in pixels
-     */
-    public void moveHorizontal(int distance){
-        erase();
-        xPosition += distance;
-        draw();
-    }
-
-    /**
-     * Move the triangle vertically.
-     * @param distance the desired distance in pixels
-     */
-    public void moveVertical(int distance){
-        erase();
-        yPosition += distance;
-        draw();
-    }
-
-    /**
-     * Slowly move the triangle horizontally.
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveHorizontal(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        } else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            xPosition += delta;
-            draw();
-        }
-    }
-
-    /**
-     * Slowly move the triangle vertically.
-     * @param distance the desired distance in pixels
-     */
-    public void slowMoveVertical(int distance){
-        int delta;
-
-        if(distance < 0) {
-            delta = -1;
-            distance = -distance;
-        } else {
-            delta = 1;
-        }
-
-        for(int i = 0; i < distance; i++){
-            yPosition += delta;
-            draw();
-        }
-    }
 
     /**
      * Change the size to the new size
@@ -146,20 +38,11 @@ public class Triangle{
         draw();
     }
     
-    /**
-     * Change the color. 
-     * @param color the new color. Valid colors are "red", "yellow", "blue", "green",
-     * "magenta" and "black".
-     */
-    public void changeColor(String newColor){
-        color = newColor;
-        draw();
-    }
 
     /*
      * Draw the triangle with current specifications on screen.
      */
-    private void draw(){
+    public void draw(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
             int[] xpoints = { xPosition, xPosition + (width/2), xPosition - (width/2) };
@@ -172,7 +55,7 @@ public class Triangle{
     /*
      * Erase the triangle on screen.
      */
-    private void erase(){
+    public void erase(){
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
             canvas.erase(this);
